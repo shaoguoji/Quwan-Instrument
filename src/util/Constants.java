63c8b779ca68c923af3ac127dd42d3b60cb10f09
Jigiang <a302748117@qq.com>
2016-11-07 23:44:56 +0800

    添加几个dao和servlet

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -3,4 +3,6 @@ package util;
 public class Constants {
 	public final static short CHANGE_PRODUCT = 0;
 	public final static short ADD_PRODUCT = 1;
+	public final static short BACK_MONEY = 2;
+	public final static short BACK_PRODUCT = 3;
 }
