39c431069d1b0584281537219b5a80eea7ff1271
Jigiang <a302748117@qq.com>
2016-11-09 17:15:16 +0800

    没什么

--- src/servlet/AdminVipServlet.java
+++ src/servlet/AdminVipServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.DealShoppingDao;
+import dao.DealshoppingDao;
 import dao.SuperAdminDao;
 import dao.VipAdminDao;
 
