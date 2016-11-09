23e1a15b5fbdfd91920054607d863cc24a6f1348
caikaizhen <270495674@qq.com>
2016-11-09 16:50:04 +0800

    随便

--- src/dao/Admin_Dao.java
+++ src/dao/Admin_Dao.java
@@ -9,6 +9,7 @@ import java.util.List;
 import util.DBHelper;
 
 import entity.Admin;
+import entity.Users;
 
 public class Admin_Dao {
 	public static List<Admin>  depts;
@@ -28,6 +29,49 @@ public class Admin_Dao {
 				depts.add(dept); 
 			}
 		return depts;
+	}
+	
+	public Admin getItemsByAdminUsername(String admin_username) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBHelper.getConnection();
+			String sql = "select * from admin where admin_username=?"; // SQL语句
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, admin_username);
+			rs = stmt.executeQuery();
+			if (rs.next()) {
+				Admin dept = new Admin(rs.getInt("admin_id"),rs.getString("admin_username"),rs.getString("admin_password"),rs.getString("admin_dep"),rs.getInt("admin_level"));
+				return dept;
+			} else {
+				return null;
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		} finally {
+			// 释放数据集对象
+			if (rs != null) {
+				try {
+					rs.close();
+					rs = null;
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+			// 释放语句对象
+			if (stmt != null) {
+				try {
+					stmt.close();
+					stmt = null;
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+
+		}
 	}
 	
 	
